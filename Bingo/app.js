d6b110bd9cdc047ddc197047ccc03508991d91eb
Hen Friedmann <99195954+friedmannhen@users.noreply.github.com>
2023-05-22 18:05:51 +0300

    Added mute option and design fixes

--- Bingo/app.js
+++ Bingo/app.js
@@ -28,18 +28,18 @@ function generateRandomNumber() {
 // Generate a new number and display it
 function generateNumber() {
   drums.play();
+  document.querySelector("*").style.cursor = "wait";
   roll.classList.remove("roll-btn-show");
   roll.classList.add("roll-btn-hide");
   numberDisplay.classList.add("show");
   document.querySelector(".reset").disabled = true;
   // Check if all numbers have been picked
 
-
   // Disable generate number button during animation
   var generateButton = document.querySelector("button");
   generateButton.disabled = true;
 
-  // Start displaying random numbers 
+  // Start displaying random numbers
   var startTime = Date.now();
   var interval = setInterval(function () {
     // Generate a new random number
@@ -80,6 +80,8 @@ function generateNumber() {
     roll.classList.add("roll-btn-show");
     numberDisplay.classList.remove("show");
     pop.play();
+  document.querySelector("*").style.cursor = "default";
+
   }, 5000);
 }
 
@@ -100,38 +102,41 @@ function resetGame() {
   numberList.innerHTML = "";
 }
 
-
-// cursour animation 
+// cursour animation
 
 // create instance of kinet with custom settings
 var kinet = new Kinet({
-  acceleration: 0.09,
+  acceleration: 0.1,
   friction: 0.15,
   names: ["x", "y"],
 });
 
 // select circle element
-var circle = document.getElementById('circle');
+var circle = document.getElementById("circle");
 
 // set handler on kinet tick event
-kinet.on('tick', function(instances) {
-  circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
+kinet.on("tick", function (instances) {
+  circle.style.transform = `translate3d(${instances.x.current}px, ${
+    instances.y.current
+  }px, 0) rotateX(${instances.x.velocity / 2}deg) rotateY(${
+    instances.y.velocity / 2
+  }deg)`;
 });
 
 // call kinet animate method on mousemove
-document.addEventListener('mousemove', function (event) {
-  kinet.animate('x', event.clientX - window.innerWidth/2);
-  kinet.animate('y', event.clientY - window.innerHeight/2-20);
+document.addEventListener("mousemove", function (event) {
+  kinet.animate("x", event.clientX - window.innerWidth / 2);
+  kinet.animate("y", event.clientY - window.innerHeight / 2 - 20);
 });
 
-// var mutebtn = document.getElementById("mute");
-// function mute() {
-//   drums.mute = !drums.mute;
-//   pop.mute = !pop.mute;
-//   if (drums.mute && pop.mute) {
-//     btn.innerHTML = '<i class="fa-solid fa-video "></i>';
-//   } else {
-//     video.pause();
-//     btn.innerHTML = '<i class="fa-solid fa-video-slash "></i>';
-//   }
-// }
+var mutebtn = document.getElementById("mute");
+function mute() {
+  drums.muted =  !(drums.muted)
+  pop.muted = !(pop.muted)
+
+  if (drums.muted && pop.muted) {
+    mutebtn.classList.replace("fa-volume-high", "fa-volume-xmark");
+  } else {
+    mutebtn.classList.replace("fa-volume-xmark", "fa-volume-high");
+  }
+}
